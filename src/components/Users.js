65801300774b5f31495a446193dcc6bed3e9b466
Jethro Williams <jethrowilliams@outlook.com>
2021-10-21 17:58:21 +0100

    Change a functional component which uses state, to be a class-based component

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Component } from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
@@ -9,29 +9,48 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ];
 
-const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
-
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
+// Prior to React 16.8 (which introduced hooks) you could only manage state in class-based components (and not in functional components)
+class Users extends Component {
+  // The constructor is automatically called when this class is instantiated (whenever it gets used as a component)
+  constructor() {
+    super(); // When you add 'constructor()' to your class AND you extend another class ('extends Component' above) then you need to call 'super()', which calls the constructor of the parent (super) class
+    this.state = {
+      showUsers: true,
+      anotherPieceOfState: 'Example',
+    };
+    // In class-based components, your state is ALWAYS an object (not a string, boolean, array etc).
+    // It also HAS TO be a property named 'state', and you can only have one state object (which contains all the different 'pieces' of state)
   };
 
-  const usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
+  toggleUsersHandler() {
+    this.setState((curState) => {
+      return { showUsers: !curState.showUsers };
+    });
+    // setState always takes an object (and if it uses the current state (as here), it should be a function, which returns an object)
+    // However, it WON'T OVERRIDE THE EXISTING STATE
+    // Instead it will merge your new state object with the existing state object
+    // So, for example, 'this.setState({ someNewState: 'WTF?' })' would update the state (as set in the constructor) to be:
+    // { showUsers: true, anotherPieceOfState: 'Example', someNewState: 'WTF?' }
+  };
 
-  return (
-    <div className={classes.users}>
-      <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
-      </button>
-      {showUsers && usersList}
-    </div>
-  );
+  render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}> {/* Passing 'this' as an argument with bind, ensures that in the 'togglerUsersHandler' function, 'this' refers to the surrounding class (it otherwise wouldn't) */}
+          {this.state.showUsers ? 'Hide' : 'Show'} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  };
 };
 
 export default Users;
